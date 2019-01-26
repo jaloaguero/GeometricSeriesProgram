07b752311c6ee814509bfef7ba3202755527abc5
Mia S Benedek <45770536+mimiandboo@users.noreply.github.com>
2019-01-26 15:44:08 -0600

    Add files via upload
    opening_and_implement_file_Mia_version1.1 is implemented
    this code finds the y intercept of equation and coefficient

---
+++ simple_equation_method.py
@@ -0,0 +1,52 @@
+#! /usr/bin/python
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "mia"
+__date__ = "$Jan 25, 2019 12:53:39 AM$"
+
+# file variable reads testfile
+file = open("testfile", "r")
+
+# initialized empty array content
+content = []
+
+# for loop to read all elements as int
+for val in file.read().split():
+    content.append(int(val))
+
+# closes file
+file.close()
+
+for x in range(len(content)):
+    print(content[x])
+
+# **insert quicksort method here**
+
+# assuming array is sorted:
+
+# y_int variable for y intercept of the equation
+# intercept is found where x = 0 (array[0] = y_int
+y_int = content[0]
+
+# prints out y intercept
+print("The y intercept is " + str(y_int))
+
+# for loop to subtract y intercept from all elements of the array
+for x in range(len(content)):
+    content[x] -= y_int
+    print(content[x])
+
+# @ content[1] = coefficient of x
+coefficient = content[1]
+
+# print out coefficient
+print("The coefficient is " + str(coefficient))
+
+# now divide all elements of the array by the coefficient
+for x in range(len(content)):
+    content[x] = int(content[x] / coefficient)
+    print(content[x])
+
