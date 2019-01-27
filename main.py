8cd6749a1def189713d65ba7259cf760709cbde4
Mia S Benedek <45770536+mimiandboo@users.noreply.github.com>
2019-01-27 02:19:25 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
+#! /usr/bin/python
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+#
+# solves all equations with one instance of x^n
+
 import math
-from fractions import Fraction
 
 from tkinter import *
 
-#SMALL BUG:: to continue the project, enter your numbers, press enter, then close the window. the program will continue after being closed.
+# SMALL BUG:: to continue the project, enter your numbers, press enter, then
+# close the window. the program will continue after being closed.
 
+# __author__ = "jalo"
+# __date__ = "$Jan 26, 2019 - Jan 27, 2019"
+
+# file variable writes to UserNumber
 file = open("UserNumber", "w+")
+
 
 master = Tk()
 
-output = Label(master,text = "please enter pattern numbers seperated by spaces. When you are done typing in your numbers, press Enter.")
+output = Label(master, text = "Please enter pattern numbers separated by spaces. "
+                              "When you are done typing in your numbers, press Enter.")
 output.pack()
 
 UserInput = Entry(master)
 UserInput.pack()
 
+
 def setToArray(UI):
     UINum = UserInput.get()
-    #print(UINum)
     file.write(UINum)
-    master.quit()
-
+    master.destroy()
 
 UserInput.bind('<Return>', setToArray)
 
@@ -28,96 +42,108 @@ master.mainloop()
 
 file.close()
 
-#===============================================
+#=================================================================
 
-__author__ = "mia"
-__date__ = "$Jan 25, 2019 12:53:39 AM$"
+# __author__ = "mia"
+# __date__ = "$Jan 26, 2019 - Jan 27, 2019"
 
-# file variable reads testfile
+# file variable reads UserNumber
 file = open("UserNumber", "r")
 
 # initialized empty array content
 content = []
 
-# for loop to read all elements as int
+# for loop to read all elements from the file as float
 for val in file.read().split():
+    # takes file elements and saves into array content
     content.append(float(val))
 
 # closes file
 file.close()
 
 # y_int variable for y intercept of the equation
-y_int = 1
+# intercept is found where x = 0 (array[0] = y_int)
 y_int = content[0]
-
-# prints out y intercept
-print("The y intercept is " + str(y_int))
 
 # for loop to subtract y intercept from all elements of the array
 for x in range(len(content)):
     content[x] -= y_int
 
-# @ content[1] = coefficient of x
+# coefficient will equal content[1]
 coefficient = content[1]
-
-# print out coefficient
-print("The coefficient is " + str(coefficient))
 
 # now divide all elements of the array by the coefficient
 for x in range(len(content)):
     content[x] = float(content[x] / coefficient)
 
-power = 0
-
-power = math.log(content[2], 2)
+# temp variable is initialized to the logbase2(content[2])
+temp = math.log(content[2], 2)
+# i variable is initialized to 1
+# i will be used to find the denominator
 i = 1
-temp = power
+# power variable will hold the exponent
+power = temp
 
-while not round(power * i, 2).is_integer():
+# while the (temp * i) rounded to 2 decimal places
+# is not an integer, then increment i, power now holds temp * i
+while not round(temp * i, 2).is_integer():
     i += 1
-    temp = power * i
+    power = temp * i
 
-print(str(round(temp, 2)) + "/" + str(i))
+teststr = ""
 
 # checks the coefficient
+# when coefficient == 1
 if coefficient == 1:
-    print("y = " + "x", end = "")
+    teststr += "y = x"
+# when coefficient == -1
 elif coefficient == -1:
-    print("y = -" + "x", end = "")
+    teststr += "y = -x"
+# when coefficient is an integer
 elif coefficient.is_integer():
-    print("y = " + str(int(coefficient)) + "x", end = "")
+    teststr += "y = " + str(int(coefficient)) + "x"
+# when coefficient is not an integer
 else:
-    print("y = " + str(coefficient) + "x", end = "")
+    teststr += "y = " + str(coefficient) + "x"
 
-# checks the exponent
-if temp == 1:
-    print(" ", end = "")
-elif temp.is_integer():
-    print("^" + str(int(temp)) + " ", end = "")
+# checks the exponent (aka power)
+# when the power == 1
+if power == 1:
+    teststr += " "
+# when the power is an integer
+elif power.is_integer():
+    teststr += "^" + str(int(power)) + " "
+# when the power is not an integer
 else:
-    print("^(" + str(int(round(temp, 2))) + "/" + str(i) + ") ", end = "")
+    teststr += "^(" + str(int(round(power, 2))) + "/" + str(i) + ") "
 
 # checks the y intercept
+# when y int == 0
 if y_int == 0:
-    print()
+    teststr += ""
+# when y int < 0 and is an integer
 elif y_int < 0 & y_int.is_integer():
-    print("- " + str(int(-1 * y_int)))
+    teststr += "- " + str(int(-1 * y_int))
+# when y int < 0 and is not an integer
 elif y_int < 0 & (not y_int.is_integer()):
-    print("- " + str(-1 * y_int))
+    teststr += "- " + str(-1 * y_int)
+# when y > 0 and is an integer
 elif y_int.is_integer():
-    print("+ " + str(int(y_int)))
+    teststr += "+ " + str(int(y_int))
+# when y > 0 and is not an integer
 else:
-    print("+ " + str(y_int))
+    teststr += "+ " + str(y_int)
+
+# final equation has format y = mx^n + b
 
 #========================================================================
 
 next = Tk()
 
-
-ou2 = Label(next, text = "Final equation y = " + str(coefficient) + "x")
+ou2 = Label(next, text = "Final equation " + teststr)
 ou2.pack()
 
-ou3 = Label(next, text = "lets break this down")
+ou3 = Label(next, text = "Let's break this down")
 ou3.pack()
 
 
